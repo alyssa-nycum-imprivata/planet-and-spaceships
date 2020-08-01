4bb6b0e51bb6256c144c4e58de70daa1df92f3da
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-08-01 14:20:00 -0500

    finished first 2 requirements of practice

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace planet_and_spaceships
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+
+            // Add() Jupiter and Saturn at the end of the list
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            // test
+            // planetList.ForEach(planet => Console.WriteLine(planet));
+
+            // Create another List that contains that last two planet of our solar system.
+
+            List<string> planetList2 = new List<string>(){"Uranus", "Neptune"};
+
+            // test
+            // planetList2.ForEach(planet => Console.WriteLine(planet));
+
+            
+        }
+    }
+}
